d2886215eb35de11ed4bbbb3c912a2e6cccf5e86
Youmin-Dev <kimyoumin.dev@gmail.com>
2025-05-30 12:22:15 +0900

    Fixed getch

--- C-Learn/2025.05.30/getch.c
+++ C-Learn/2025.05.30/getch.c
@@ -5,7 +5,7 @@
 void Talk()
 {
 	char name[100]; // can saved text max number
-	printf("Who's coming\n");
+	printf("\nWho's coming\n");
 	Sleep(1000);
 	printf("Nice to meet ya, What about you?\n What's your name?\n");
 	printf("What did you say?\n>");
@@ -18,22 +18,26 @@ int main()
 {
 	int time = 1;
 	char choice;
-	choice = _getch();
 	printf("Do you want Talk?\ny/n >");
+	choice = _getch();
+
 	while (time)
 	{
 		if (choice == 'y')
 		{
+			time = 0;
 			Talk();
 		}
 		else if (choice == 'n')
 		{
-			printf("Um.. ok, See you next time :)\n");
+			printf("\nUm.. ok, See you next time :)\n");
 			time = 0;
 		}
 		else
 		{
-			printf("Sorry? again plese..\n>");
+			printf("Sorry? again plese..\n");
+			printf("\nDo you want Talk?\ny/n >");
+			choice = _getch();
 		}
 	}
 	return 0;
