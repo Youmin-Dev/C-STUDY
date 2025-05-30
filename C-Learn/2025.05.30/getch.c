1e7e5afcc4ac894abdd0357047bce12a583f9256
Youmin-Dev <kimyoumin.dev@gmail.com>
2025-05-30 10:35:56 +0900

    getch

---
+++ C-Learn/2025.05.30/getch.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <conio.h>
+#include <windows.h>
+
+void Talk()
+{
+	char name[100]; // can saved text max number
+	printf("Who's coming\n");
+	Sleep(1000);
+	printf("Nice to meet ya, What about you?\n What's your name?\n");
+	printf("What did you say?\n>");
+	scanf_s("%s", name, 100); //scanf + _s, finally saved max number 
+	printf("Oh nice to meet ya, %s", name);
+}
+
+
+int main()
+{
+	Talk();
+	return 0;
+}
